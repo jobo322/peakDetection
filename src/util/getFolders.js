b10a206a29f9eeafd0efa4a4111189c52cec791f
jwist <julien.wist@murdoch.edu.au>
2021-04-14 08:26:12 +0000

    fix: pay attention to root in linux

--- src/util/getFolders.js
+++ src/util/getFolders.js
@@ -9,21 +9,25 @@ module.exports = function getFolders(pathFolder, options = {}) {
   let folders = [];
   let quantFactorSample = {};
   let directories = walker(pathFolder, { nofile: true });
-
   for (let i = 0; i < directories.length; i++) {
     let dir = directories[i].path;
+console.log(join(dir, '1r'))
     if (!existsSync(join(dir, '1r'))) continue;
+//console.log(`pasa first exist`)
     let parts = dir.split(separator);
     if (Number(parts[parts.length - 1]) > 10) continue;
     let quantFactorPath = join(
+      parts[0][0] !== dir[0] ? '/':'',
       ...parts.slice(0, parts.length - 2),
       'QuantFactorSample.xml',
     );
+//console.log(`path q ${quantFactorPath}`);
     if (!existsSync(quantFactorPath)) continue;
-    
+//    console.log('pasa quant exist');
     quantFactorSample[dir] = extractEreticFactor(readFileSync(quantFactorPath, 'utf8'));
     folders.push(dir);
   }
+//console.log(folders)
   return { folders, quantFactorSample };
 };
 
