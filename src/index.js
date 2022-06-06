b34a06e49a76f42624bee2c445a2b21749b76a7e
jobo322 <jose.bolanos@correounivalle.edu.co>
2022-06-06 13:25:30 +0200

    feat: generate ROIs from csv

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { writeFileSync } = require('fs');
+const { readFileSync, writeFileSync } = require('fs');
 const { join } = require('path');
 
 const { convertFileList } = require('brukerconverter');
@@ -18,16 +18,21 @@ const { SpectrumGenerator } = require('spectrum-generator');
 const path = 'C:\\Users\\alejo\\Documents\\BIOGUNE\\20S00399_test';
 const pathToWrite = './';
 
-let ROI = [
-  { from: 2.0, to: 2.1 },
-  { from: 3.0, to: 3.2 },
-];
+const csvData = readFileSync('./src/annotationDB.csv', 'utf-8');
+const database = getJSON(csvData);
+const ROI = getROIs(database, 'urine', [
+  { name: 'delta [ppm]', saveAs: 'delta' },
+  { name: 'from [ppm]', saveAs: 'from' },
+  { name: 'to [ppm]', saveAs: 'to' },
+]);
 
 let process2DOptions = {
   zonesPicking: {
     tolerances: [5, 100],
+    nuclei: ['1H', '1H'],
+    realTopDetection: false,
   },
-  jResAnalyzer: { getZones: true },
+  jResAnalyzer: { reference: 0, getZones: true },
 };
 
 let gsdOptions = {
@@ -93,6 +98,80 @@ async function main() {
 }
 
 main();
+
+function getROIs(data, target, mapping) {
+  let rois = [];
+  for (let roi of data) {
+    if (roi[target]) {
+      const targetData = roi[target];
+      let tmp = {};
+      if (Number.isFinite(targetData[mapping[0].name])) {
+        for (let map of mapping) {
+          tmp[map.saveAs] = targetData[map.name];
+        }
+        rois.push(tmp);
+      }
+    }
+  }
+  return rois;
+}
+function getJSON(data) {
+  const lines = data.split('\n');
+  let headers = [];
+  let secondHeaders = [];
+  let secondHeadersData = lines[1].split(',');
+  const firstHeadersData = lines[0].split(',');
+  for (let i = 0; i < firstHeadersData.length; i++) {
+    const cell = firstHeadersData[i];
+    if (cell.length > 0) {
+      headers.push({
+        fromIndex: i,
+        name: cell,
+      });
+    }
+  }
+
+  for (let i = 1; i < headers.length - 1; i++) {
+    headers[i - 1].toIndex = headers[i].fromIndex - 1;
+  }
+
+  for (let cell of secondHeadersData) {
+    secondHeaders.push({
+      name: cell.replace('\r', ''),
+    });
+  }
+
+  const getHeaderName = (headers, index) => {
+    for (const header of headers) {
+      const { fromIndex, toIndex } = header;
+      if (index >= fromIndex && index <= toIndex) return header.name;
+    }
+    return 'null';
+  };
+
+  const getValue = (str) => {
+    if (str.length === 0) return str;
+    return !isNaN(Number(str)) ? Number(str) : str;
+  };
+
+  let metabolites = [];
+  for (let i = 2; i < lines.length; i++) {
+    const cells = lines[i].split(',');
+    let metabolite = {};
+    for (let j = 0; j < secondHeaders.length; j++) {
+      let headerName = getHeaderName(headers, j);
+      if (!metabolite[headerName]) metabolite[headerName] = {};
+      metabolite[headerName][secondHeaders[j].name] = getValue(
+        cells[j].replace('\r', ''),
+      );
+    }
+    metabolite = { ...metabolite.null, ...metabolite };
+    delete metabolite.null;
+    metabolites.push(metabolite);
+  }
+
+  return metabolites;
+}
 
 function align(input) {
   const { spectrum, referencePeaks, delta, fromTo } = input;
@@ -165,10 +244,11 @@ function process1D(data) {
       fitted: spectrumGenerator.getSpectrum(),
     });
   }
+  return peakOptimized;
 }
 
 function process2D(data, options) {
-  const { zonesPicking, jRestAnalyzer } = options;
+  const { zonesPicking, jResAnalyzer } = options;
 
   const frequency = data.meta.observeFrequency;
   const minMax = data.minMax;
@@ -179,7 +259,12 @@ function process2D(data, options) {
 
   const newZones = [];
   for (let zone of zones) {
-    newZones.push(xyzJResAnalyzer(zone.signals, jRestAnalyzer));
+    newZones.push(
+      xyzJResAnalyzer(zone.signals, {
+        observedFrequencies: [frequency, frequency],
+        ...jResAnalyzer,
+      }),
+    );
   }
 
   return {
